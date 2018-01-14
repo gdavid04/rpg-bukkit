cda83efc3b44b2cfe780d6990eca3d8ab27eb9bb
GDavid04 <Gabordavid04@gmail.com>
2018-01-14 19:04:53 +0100

    removed some unnecessary parts, changed package name

--- src/main/java/gdavid/rpg/RPG.java
+++ src/main/java/gdavid/rpg/RPG.java
@@ -1,4 +1,4 @@
-package me.gdavid.rpg;
+package gdavid.rpg;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -33,7 +33,7 @@ public final class RPG extends JavaPlugin implements Listener {
 		createDisplay(Material.IRON_SWORD, classGUI, 0, getConfig().getString("lang.warrior"), warriorlore);
 		ArrayList<String> archerlore = (ArrayList<String>) getConfig().getStringList("lang.archer-desc");
 		createDisplay(Material.BOW, classGUI, 1, getConfig().getString("lang.archer"), archerlore);
-		this.saveDefaultConfig();
+		saveDefaultConfig();
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
@@ -64,17 +64,17 @@ public final class RPG extends JavaPlugin implements Listener {
 		Inventory inventory = event.getInventory(); // The inventory that was clicked in
 		if (inventory.getName().equals(getConfig().getString("lang.select-class"))) {
 			if (event.getSlot() == 0) {
-				this.getConfig().set("players." + player.getName() + ".class", "warrior");
-				this.getConfig().set("players." + player.getName() + ".level", 1);
-				this.getConfig().set("players." + player.getName() + ".xp", 0);
-				this.saveConfig();
+				getConfig().set("players." + player.getName() + ".class", "warrior");
+				getConfig().set("players." + player.getName() + ".level", 1);
+				getConfig().set("players." + player.getName() + ".xp", 0);
+				saveConfig();
 				player.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "Your class has been changed to warrior");
 				player.closeInventory();
 			} else if (event.getSlot() == 1) {
-				this.getConfig().set("players." + player.getName() + ".class", "archer");
-				this.getConfig().set("players." + player.getName() + ".level", 1);
-				this.getConfig().set("players." + player.getName() + ".xp", 0);
-				this.saveConfig();
+				getConfig().set("players." + player.getName() + ".class", "archer");
+				getConfig().set("players." + player.getName() + ".level", 1);
+				getConfig().set("players." + player.getName() + ".xp", 0);
+				saveConfig();
 				player.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "Your class has been changed to archer");
 				player.closeInventory();
 			}
@@ -87,18 +87,18 @@ public final class RPG extends JavaPlugin implements Listener {
 		if (cmd.getName().equalsIgnoreCase("rpg")) {
 			if (args.length == 1) {
 				if (args[0].equalsIgnoreCase("class") && sender instanceof Player) {
-					if (this.getConfig().isSet("players." + ((Player)sender).getName() + ".class") == false) {
+					if (getConfig().isSet("players." + ((Player)sender).getName() + ".class") == false) {
 						((Player) sender).openInventory(classGUI);
 					} else {
 						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.RED + "You have already chosed a class!");
 					}
 				} else if (args[0].equalsIgnoreCase("stats") && sender instanceof Player) {
-					if (this.getConfig().isSet("players." + ((Player)sender).getName() + ".class") == true) {
+					if (getConfig().isSet("players." + ((Player)sender).getName() + ".class") == true) {
 						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "Your stats:");
-						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "Class: " + this.getConfig().getString("players." + ((Player)sender).getName().toString() + ".class"));
-						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "Level " + this.getConfig().getInt("players." + ((Player)sender).getName().toString() + ".level"));
-						if (this.getConfig().getInt("players." + ((Player)sender).getName().toString() + ".level") < this.getConfig().getInt("max-level")) {
-							sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "XP: " + this.getConfig().getInt("players." + ((Player)sender).getName() + ".xp") + "/" + xpToNext(((Player)sender).getName()));
+						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "Class: " + getConfig().getString("players." + ((Player)sender).getName().toString() + ".class"));
+						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "Level " + getConfig().getInt("players." + ((Player)sender).getName().toString() + ".level"));
+						if (getConfig().getInt("players." + ((Player)sender).getName().toString() + ".level") < getConfig().getInt("max-level")) {
+							sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.WHITE + "XP: " + getConfig().getInt("players." + ((Player)sender).getName() + ".xp") + "/" + xpToNext(((Player)sender).getName()));
 						} else {
 							sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "You have reached the maximum level!");
 						}
@@ -114,34 +114,15 @@ public final class RPG extends JavaPlugin implements Listener {
 				sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "Help");
 				sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "/rpg class - chose class");
 				sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "/rpg stats - your stats");
-			}/* else if (args.length == 2 && args[0].equalsIgnoreCase("class") && sender instanceof Player) {
-				/*if (this.getConfig().isSet("players." + ((Player)sender).getName() + ".class") == false) {
-					if (args[1].equalsIgnoreCase("warrior")) {
-						this.getConfig().set("players." + ((Player)sender).getName() + ".class", "warrior");
-						this.getConfig().set("players." + ((Player)sender).getName() + ".level", 1);
-						this.getConfig().set("players." + ((Player)sender).getName() + ".xp", 0);
-						this.saveConfig();
-						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "Your class has been changed to warrior");
-					} else if (args[1].equalsIgnoreCase("archer")) {
-						this.getConfig().set("players." + ((Player)sender).getName() + ".class", "archher");
-						this.getConfig().set("players." + ((Player)sender).getName() + ".level", 1);
-						this.getConfig().set("players." + ((Player)sender).getName() + ".xp", 0);
-						this.saveConfig();
-						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.GREEN + "Your class has been changed to archer.");
-					} else {
-						sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.RED + "This class doesn't exist! /rpg class for classes");
-					}
-				} else {
-					sender.sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.RED + "You have already selected a class!");
-				}
-			}*/
+			}
 		}
 		return false; 
 	}
 	
+	//TODO replace deprecated methods
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		if (this.getConfig().isSet("players." + event.getPlayer().getName() + ".class") == false) {
+		if (getConfig().isSet("players." + event.getPlayer().getName() + ".class") == false) {
 			event.getPlayer().sendMessage(ChatColor.GOLD + "[RPG] " + ChatColor.RED + getConfig().getString("lang.no-class"));
 		}
 		event.getPlayer().setMaxHealth(maxHealth(event.getPlayer().getName()));
@@ -160,10 +141,10 @@ public final class RPG extends JavaPlugin implements Listener {
 			crit = 1.4 + (getRandom(0, 6) / (double)10);
 		}
 		if (damager instanceof Player) {
-			if (this.getConfig().isSet("players." + ((Player)damager).getName() + ".class")) {
-				switch (this.getConfig().getString("players." + ((Player)damager).getName() + ".class")) {
+			if (getConfig().isSet("players." + ((Player)damager).getName() + ".class")) {
+				switch (getConfig().getString("players." + ((Player)damager).getName() + ".class")) {
 				case "warrior":
-					event.setDamage(event.getDamage() + 1 + (this.getConfig().getInt("players." + ((Player)damager).getName() + ".level") * this.getConfig().getDouble("levelup-atk-increase")) * crit);
+					event.setDamage(event.getDamage() + 1 + (getConfig().getInt("players." + ((Player)damager).getName() + ".level") * getConfig().getDouble("levelup-atk-increase")) * crit);
 					((Player)damager).sendMessage(ChatColor.GOLD + "[RPG] " + (crit < 1.4 ? ChatColor.GRAY : ChatColor.GOLD) + "You dealt " + event.getDamage() + " HP damage!");
 					giveXp((int) event.getDamage(), ((Player)damager).getName());
 					break;
@@ -176,13 +157,13 @@ public final class RPG extends JavaPlugin implements Listener {
 			if (((Arrow)damager).getShooter() instanceof Player) {
 				Player shooter = (Player) ((Arrow)damager).getShooter();
 				
-				if (this.getConfig().isSet("players." + (shooter.getName() + ".class"))) {
-					switch (this.getConfig().getString("players." + shooter.getName() + ".class")) {
+				if (getConfig().isSet("players." + (shooter.getName() + ".class"))) {
+					switch (getConfig().getString("players." + shooter.getName() + ".class")) {
 					case "warrior":
 						//warrior has no ranged boost
 						break;
 					case "archer":
-						event.setDamage(event.getDamage() + 1 + (this.getConfig().getInt("players." + shooter.getName() + ".level") * this.getConfig().getDouble("levelup-atk-increase")) * crit);
+						event.setDamage(event.getDamage() + 1 + (getConfig().getInt("players." + shooter.getName() + ".level") * getConfig().getDouble("levelup-atk-increase")) * crit);
 						((Player)damager).sendMessage(ChatColor.GOLD + "[RPG] " + (crit < 1.4 ? ChatColor.GRAY : ChatColor.GOLD) + "Your arrow dealt " + event.getDamage() + " HP damage!");
 						giveXp((int) event.getDamage(), shooter.getName());
 						break;
@@ -199,35 +180,36 @@ public final class RPG extends JavaPlugin implements Listener {
 	    return new Random().nextInt((upper - lower) + 1) + lower;
 	}
 	
+	//TODO replace deprecated methods
 	public void giveXp(int amount, String name) {
-		Player damager = this.getServer().getPlayer(name);
-		if (/*perm check*/ true){
-			if (this.getConfig().getInt("players." + name + ".level") >= this.getConfig().getInt("max-level")) return;
-			this.getConfig().set("players." + name + ".xp", this.getConfig().getInt("players." + name + ".xp") + amount);
-			if (this.getConfig().getInt("players." + name + ".xp") > xpToNext(name)) {
-				this.getConfig().set("players." + name + ".xp", this.getConfig().getInt("players." + name + ".xp") - xpToNext(name));
-				this.getConfig().set("players." + name + ".level", this.getConfig().getInt("players." + name + ".level") + 1);
-				//getServer().getPlayer(name).sendMessage(ChatColor.GOLD + "[RPG] Level up! Level: " + this.getConfig().getInt("players." + name + ".level"));
+		Player damager = getServer().getPlayer(name);
+		if (/*TODO permission check*/ true){
+			if (getConfig().getInt("players." + name + ".level") >= getConfig().getInt("max-level")) return;
+			getConfig().set("players." + name + ".xp", getConfig().getInt("players." + name + ".xp") + amount);
+			if (getConfig().getInt("players." + name + ".xp") > xpToNext(name)) {
+				getConfig().set("players." + name + ".xp", getConfig().getInt("players." + name + ".xp") - xpToNext(name));
+				getConfig().set("players." + name + ".level", getConfig().getInt("players." + name + ".level") + 1);
 				getServer().getPlayer(name).setMaxHealth(maxHealth(name));
 				getServer().getPlayer(name).setHealth(getServer().getPlayer(name).getMaxHealth());
-				getServer().getPlayer(name).sendTitle(getConfig().getString("lang.level-up"), getConfig().getString("lang.level") + " " + this.getConfig().getInt("players." + name + ".level"));
+				getServer().getPlayer(name).sendTitle(getConfig().getString("lang.level-up"), getConfig().getString("lang.level") + " " + getConfig().getInt("players." + name + ".level"));
 			}
-			this.saveConfig();
+			saveConfig();
 		}
 	}
 	
+	//TODO replace deprecated methods
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
-    	event.getPlayer().setMaxHealth(maxHealth(event.getPlayer().getName()));
-    	event.getPlayer().setHealth(event.getPlayer().getMaxHealth());
+    		event.getPlayer().setMaxHealth(maxHealth(event.getPlayer().getName()));
+    		event.getPlayer().setHealth(event.getPlayer().getMaxHealth());
     }
     
     private double maxHealth(String name) {
-		return (this.getConfig().getInt("players." + name + ".level")-1) * this.getConfig().getDouble("levelup-hp-increase") + 20;
+		return (getConfig().getInt("players." + name + ".level")-1) * getConfig().getDouble("levelup-hp-increase") + 20;
 	}
 
 	private int xpToNext(String name) {
-		return ((this.getConfig().getInt("players." + name + ".level")-1) * this.getConfig().getInt("levelup-xp-increase")) + this.getConfig().getInt("levelup-xp-base");
+		return ((getConfig().getInt("players." + name + ".level")-1) * getConfig().getInt("levelup-xp-increase")) + getConfig().getInt("levelup-xp-base");
 	}
 
 	@Override
